5b5b4135363baa4cddd3c3e7b689f43b38653362
tcorbat <thomas.corbat@gmail.com>
2019-03-09 19:10:51 +0100

    sphere tests (page 69)

--- RayTracerTest/test/ShapesTestSuite.cpp
+++ RayTracerTest/test/ShapesTestSuite.cpp
@@ -6,12 +6,19 @@
 using namespace Shapes;
 
 void testDefaultInitializedSphere() {
-	Sphere const sphere{};
+	constexpr Sphere sphere{};
 	ASSERT_EQUAL(identity<4>, sphere.transform);
+}
+
+void testSphereWithCustomTransform() {
+	constexpr Matrix<4, 4> expected{2, 3, 4};
+	constexpr Sphere sphere{{}, expected};
+	ASSERT_EQUAL(expected, sphere.transform);
 }
 
 cute::suite make_suite_ShapesTestSuite() {
 	cute::suite s { };
 	s.push_back(CUTE(testDefaultInitializedSphere));
+	s.push_back(CUTE(testSphereWithCustomTransform));
 	return s;
 }
