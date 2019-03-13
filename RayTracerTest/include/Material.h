d471aa0db10773f60de8233701adba1991ad68a1
Thomas Corbat <thomas.corbat@hsr.ch>
2019-03-13 13:40:50 +0100

    make material equality comparable

--- RayTracerTest/include/Material.h
+++ RayTracerTest/include/Material.h
@@ -2,16 +2,26 @@
 #define MATERIAL_H_
 
 #include "Color.h"
+#include "DoubleComparison.h"
+#include "Operators.h"
 
-struct Material {
+struct Material : boost::equality_comparable<Material> {
 	Color color;
 	double ambient;
 	double diffuse;
 	double specular;
 	double shininess;
+
+	constexpr bool operator==(Material const & other) const {
+		return color == other.color &&
+				isEqual(ambient, other.ambient) &&
+				isEqual(diffuse, other.diffuse) &&
+				isEqual(specular, other.specular) &&
+				isEqual(shininess, other.shininess);
+	}
 };
 
-constexpr Material defaultMaterial{Colors::white, 0.1, 0.9, 0.9, 200.0};
+constexpr Material defaultMaterial{{}, Colors::white, 0.1, 0.9, 0.9, 200.0};
 
 
 #endif /* MATERIAL_H_ */
